75ee070112fb7b1cad8ec94544240bab43975acb
Valerio Franchi <vlrfranchi1@gmail.com>
2022-11-01 13:50:39 +0000

    adding user input

--- directory.rb
+++ directory.rb
@@ -1,33 +1,38 @@
-#first we print the list of students
-students = [
-{name: "Dr. Hannibal Lecter",cohort: :november},
-{name: "Darth Vader", cohort: :november},
-{name: "Nurse Ratched", cohort: :november},
-{name: "Michael Corleone", cohort: :november},
-{name: "Alex DeLarge", cohort: :november},
-{name: "The Wicked Witch of the West", cohort: :november},
-{name: "Terminator", cohort: :november},
-{name: "Freddy Krueger", cohort: :november},
-{name: "The Joker", cohort: :november},
-{name: "Joffrey Baratheon", cohort: :november},
-{name: "Norman Bates", cohort: :november},
-]
-# And we print them
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  students = []
+  name = gets.chomp
+#while the name is not empty, repeat this one
+  while !name.empty? do
+    #add a student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    #get another name from the user
+    name = gets.chomp
+  end
+  #return the array of students
+  students
+end
+
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
-# Let's iterate on the array in order to print all the students
+
 def print(students)
   students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
-# finally, we print the total number of students
+
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
+
+#first we print the list of students
